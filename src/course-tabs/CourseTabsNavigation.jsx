0905932061a1444e53a783583eb06607392f955f
Belal Sharaf <belal@sharafeldin.org>
2025-08-30 17:21:27 -0400

    Changing collapse button

--- src/course-tabs/CourseTabsNavigation.jsx
+++ src/course-tabs/CourseTabsNavigation.jsx
@@ -1,45 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useIntl } from '@edx/frontend-platform/i18n';
 import classNames from 'classnames';
+import { useIntl } from '@edx/frontend-platform/i18n';
 
 import messages from './messages';
-import Tabs from '../generic/tabs/Tabs';
-import { CoursewareSearch, CoursewareSearchToggle } from '../course-home/courseware-search';
 import { useCoursewareSearchState } from '../course-home/courseware-search/hooks';
 
-const CourseTabsNavigation = ({
-  activeTabSlug, className, tabs,
-}) => {
+const CourseTabsNavigation = ({ activeTabSlug, className, tabs }) => {
   const intl = useIntl();
   const { show } = useCoursewareSearchState();
 
   return (
-    <div id="courseTabsNavigation" className={classNames('course-tabs-navigation', className)}>
+    <div
+      id="courseTabsNavigation"
+      className={classNames('course-tabs-navigation', className, { 'has-search-open': show })}
+      data-testid="course-tabs-navigation"
+    >
       <div className="container-xl">
-        <div className="nav-bar">
-          <div className="nav-menu">
-            <Tabs
-              className="nav-underline-tabs"
-              aria-label={intl.formatMessage(messages.courseMaterial)}
+        <nav
+          className="nav-underline-tabs tabs-no-overflow"
+          role="tablist"
+          aria-label={intl.formatMessage(messages.courseMaterial)}
+        >
+          {tabs.map(({ url, title, slug }) => (
+            <a
+              key={slug}
+              href={url}
+              className={classNames('tab-link', { active: slug === activeTabSlug })}
+              data-tab={slug}
             >
-              {tabs.map(({ url, title, slug }) => (
-                <a
-                  key={slug}
-                  className={classNames('nav-item flex-shrink-0 nav-link', { active: slug === activeTabSlug })}
-                  href={url}
-                >
-                  {title}
-                </a>
-              ))}
-            </Tabs>
-          </div>
-          <div className="search-toggle">
-            <CoursewareSearchToggle />
-          </div>
-        </div>
+              <span className="label">{title}</span>
+            </a>
+          ))}
+        </nav>
       </div>
-      {show && <CoursewareSearch />}
     </div>
   );
 };
@@ -48,15 +42,10 @@ CourseTabsNavigation.propTypes = {
   activeTabSlug: PropTypes.string,
   className: PropTypes.string,
   tabs: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
+    title: PropTypes.node.isRequired,
+    slug: PropTypes.string.isRequired,
   })).isRequired,
-};
-
-CourseTabsNavigation.defaultProps = {
-  activeTabSlug: undefined,
-  className: null,
 };
 
 export default CourseTabsNavigation;
